98fbc0a4c44349e98e641b639da6960db95a0715
Y-dash <y-dash@y-dash.net>
2023-01-21 06:07:32 +0900

    1.0.0

---
+++ src/scripts/content.ts
@@ -0,0 +1,32 @@
+const FPS: number = 10;
+
+let headElement: HTMLHeadElement | null = document.getElementsByTagName('head')[0];
+let mutationObserver: MutationObserver = new MutationObserver(clickFollowingTab);
+
+if (headElement) {
+	mutationObserver.observe(headElement, {
+		childList: true,
+		subtree: true
+	});
+}
+
+clickFollowingTab();
+
+function clickFollowingTab(): void {
+	let htmlElement: HTMLElement = document.getElementsByTagName('html')[0];
+
+	if (htmlElement.dataset.ydashsExtensionTwitterNotForMeIsDone || location.href !== 'https://twitter.com/home') {
+		return;
+	}
+
+	let unselectedFollowingTabAnchorElement: HTMLAnchorElement | null = document.querySelector('div[role="tablist"] div[role="presentation"]:nth-child(2) > a[href="/home"]');
+	
+	if (unselectedFollowingTabAnchorElement == null) {
+		setTimeout(clickFollowingTab, 1000 / FPS);
+		return;
+	}
+
+	unselectedFollowingTabAnchorElement.click();
+	mutationObserver.disconnect();
+	htmlElement.dataset.ydashsExtensionTwitterNotForMeIsDone = 'true';
+}
